0f887fd3616b5798abd42566783a9874c38f68c1
Arivu <46324212+arivuselvan4210@users.noreply.github.com>
2024-02-26 20:00:42 -0800

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    origin:["blog-forntend.vercel.app"],
+    origin:["https://blog-forntend.vercel.app"],
     methods:["POST","GET"],
     credentials:true
   }
