8f75139fea0fc15aa820ddeccdec40dc44892b1a
Arivu <46324212+arivuselvan4210@users.noreply.github.com>
2024-02-29 20:32:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const blogrouter = require("./router/blog");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
-app.options('*', cors());
+app.use( cors());
 app.use("/", router);
 app.use("/blog", blogrouter);
 app.listen(5000, () => {
